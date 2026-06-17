69f80b9cbbdeee8845206fb98a1c37eb12fec6ae
Ezra <49055182+Ezrajohnny@users.noreply.github.com>
2026-06-18 02:25:31 +0530

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =
+  "https://dckecivjxpwkdotvyxum.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_w8FEEnheBDUAPVa2KGxbGQ_YEixGhQN";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
